58204e1b8a8c8e04f628b1b03b961548360f9cd7
Nathan Farlow <natethegreatest11@gmail.com>
2020-04-11 22:55:08 -0500

    Implement timer delegation

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -15,7 +15,6 @@ void init() {
 }
 
 int main() {
-
     init();
 
     Spimbot bot;
--- src/scripts/generator.py
+++ src/scripts/generator.py
@@ -1,7 +1,19 @@
 
 
-for i in range(1, 32):
-    print("sw $%d %d($k0)" % (i, i * 4))
+def print_sw_all(start_index):
+    for i in range(1, 32):
+        print("sw      $%d %d($sp)" % (i, (i - 1) * 4 + start_index))
 
-for i in range(1, 32):
-    print("sw $f%d %d($k0)" % (i, 120 + i * 4))
+    for i in range(32):
+        print("s.s     $f%d %d($sp)" % (i, 120 + (i + 1) * 4 + start_index))
+
+def print_lw_all(start_index):
+    for i in range(1, 32):
+        print("lw      $%d %d($sp)" % (i, (i - 1) * 4 + start_index))
+
+    for i in range(32):
+        print("l.s     $f%d %d($sp)" % (i, 120 + (i + 1) * 4 + start_index))
+
+if __name__ == "__main__":
+    print_sw_all(0)
+    print_lw_all(0)
