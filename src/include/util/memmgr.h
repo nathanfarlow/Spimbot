8c3e7b0bf74f70500012d0caeb002da0305e4320
Nathan Farlow <natethegreatest11@gmail.com>
2020-04-12 02:05:25 -0500

    Increase pool size

--- src/include/util/memmgr.h
+++ src/include/util/memmgr.h
@@ -63,9 +63,9 @@
 //    for single thread/task applications.
 // 
 
-#define DEBUG_MEMMGR_SUPPORT_STATS 1
+//#define DEBUG_MEMMGR_SUPPORT_STATS 1
 
-#define POOL_SIZE 8 * 1024
+#define POOL_SIZE 8 * 1024 * 1024
 #define MIN_POOL_ALLOC_QUANTAS 16
 
 
