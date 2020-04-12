fadbc2eab747d55224d5c414eac011b7634af2d1
Nathan Farlow <natethegreatest11@gmail.com>
2020-04-11 21:58:03 -0500

    Optimize interrupt handler

--- src/asm/interrupts.s
+++ src/asm/interrupts.s
@@ -1,153 +1,103 @@
-# syscall constants
-PRINT_STRING            = 4
-PRINT_CHAR              = 11
-PRINT_INT               = 1
-
-# memory-mapped I/O
-VELOCITY                = 0xffff0010
-ANGLE                   = 0xffff0014
-ANGLE_CONTROL           = 0xffff0018
-
-BOT_X                   = 0xffff0020
-BOT_Y                   = 0xffff0024
-GET_OPPONENT_HINT       = 0xffff00ec
-
-TIMER                   = 0xffff001c
-ARENA_MAP               = 0xffff00dc
-
-SHOOT_UDP_PACKET        = 0xffff00e0
-GET_BYTECOINS           = 0xffff00e4
-USE_SCANNER             = 0xffff00e8
-
-REQUEST_PUZZLE          = 0xffff00d0  ## Puzzle
-SUBMIT_SOLUTION         = 0xffff00d4  ## Puzzle
-
 BONK_INT_MASK           = 0x1000
 BONK_ACK                = 0xffff0060
 
 TIMER_INT_MASK          = 0x8000
 TIMER_ACK               = 0xffff006c
 
-REQUEST_PUZZLE_INT_MASK = 0x800       ## Puzzle
-REQUEST_PUZZLE_ACK      = 0xffff00d8  ## Puzzle
+REQUEST_PUZZLE_INT_MASK = 0x800
+REQUEST_PUZZLE_ACK      = 0xffff00d8
 
-RESPAWN_INT_MASK        = 0x2000      ## Respawn
-RESPAWN_ACK             = 0xffff00f0  ## Respawn
+RESPAWN_INT_MASK        = 0x2000
+RESPAWN_ACK             = 0xffff00f0
 
 .data
-### Puzzle
+#The interrupt handler sets these flags after acknowledging
+#the interrupt. Then userland code polls these and sets them
+#back to 0.
 has_bonk_interrupt:             .byte 0
-has_timer_interrupt:            .byte 0
 has_request_puzzle_interrupt:   .byte 0
 has_respawn_interrupt:          .byte 0
 
 .kdata
-chunkIH:    .space 40
-non_intrpt_str:    .asciiz "Non-interrupt exception\n"
-unhandled_str:    .asciiz "Unhandled interrupt type\n"
+
 .ktext 0x80000180
 interrupt_handler:
-.set noat
-    move    $k1, $at        # Save $at
-                            # NOTE: Don't touch $k1 or else you destroy $at!
-.set at
-    la      $k0, chunkIH
-    sw      $a0, 0($k0)        # Get some free registers
-    sw      $v0, 4($k0)        # by storing them to a global variable
-    sw      $t0, 8($k0)
-    #sw      $t1, 12($k0)
-    #sw      $t2, 16($k0)
-    #sw      $t3, 20($k0)
-    #sw      $t4, 24($k0)
-    #sw      $t5, 28($k0)
 
-    # Save coprocessor1 registers!
-    # If you don't do this and you decide to use division or multiplication
-    #   in your main code, and interrupt handler code, you get WEIRD bugs.
-    mfhi    $t0
-    sw      $t0, 32($k0)
-    mflo    $t0
-    sw      $t0, 36($k0)
+    #Save at and coprocessor1 registers. Use the stack pointer because a
+    #good interrupt handler should be able to interrupt itself. We may experience
+    #this if we "bonk" when we are delegating the timer interrupt to the text segment.
+    sub     $sp, $sp, 12
+
+.set noat
+    sw      $at, 0($sp)
+.set at
+
+    mfhi    $k0
+    sw      $k0, 12($sp)
+    mflo    $k0
+    sw      $k0, 16($sp)
+
 
     mfc0    $k0, $13                # Get Cause register
-    srl     $a0, $k0, 2
-    and     $a0, $a0, 0xf           # ExcCode field
-    bne     $a0, 0, non_intrpt
-
-
+    srl     $k1, $k0, 2
+    and     $k1, $k1, 0xf           # ExcCode field
+    bne     $k1, 0, done            # jump to done if no interrupt
 
 interrupt_dispatch:                 # Interrupt:
     mfc0    $k0, $13                # Get Cause register, again
     beq     $k0, 0, done            # handled all outstanding interrupts
 
-    and     $a0, $k0, BONK_INT_MASK     # is there a bonk interrupt?
-    bne     $a0, 0, bonk_interrupt
+    and     $k1, $k0, BONK_INT_MASK     # is there a bonk interrupt?
+    bne     $k1, 0, bonk_interrupt
 
-    and     $a0, $k0, TIMER_INT_MASK    # is there a timer interrupt?
-    bne     $a0, 0, timer_interrupt
+    and     $k1, $k0, TIMER_INT_MASK    # is there a timer interrupt?
+    bne     $k1, 0, timer_interrupt
 
-    and     $a0, $k0, REQUEST_PUZZLE_INT_MASK
-    bne     $a0, 0, request_puzzle_interrupt
+    and     $k1, $k0, REQUEST_PUZZLE_INT_MASK
+    bne     $k1, 0, request_puzzle_interrupt
 
-    and     $a0, $k0, RESPAWN_INT_MASK
-    bne     $a0, 0, respawn_interrupt
+    and     $k1, $k0, RESPAWN_INT_MASK
+    bne     $k1, 0, respawn_interrupt
 
-    li      $v0, PRINT_STRING       # Unhandled interrupt types
-    la      $a0, unhandled_str
-    syscall
     j       done
 
 bonk_interrupt:
     sw      $0, BONK_ACK
-    li      $t0, 1
-    sb      $t0, has_bonk_interrupt
+    li      $k0, 1
+    sb      $k0, has_bonk_interrupt
     j       interrupt_dispatch      # see if other interrupts are waiting
 
 timer_interrupt:
     sw      $0, TIMER_ACK
-    li      $t0, 1
-    sb      $t0, has_timer_interrupt
+    li      $k0, 1
+    sb      $k0, has_timer_interrupt
     j        interrupt_dispatch     # see if other interrupts are waiting
 
 request_puzzle_interrupt:
     sw      $0, REQUEST_PUZZLE_ACK
-    li      $t0, 1
-    sb      $t0, has_request_puzzle_interrupt
+    li      $k0, 1
+    sb      $k0, has_request_puzzle_interrupt
     j       interrupt_dispatch
 
 respawn_interrupt:
     sw      $0, RESPAWN_ACK
-    li      $t0, 1
-    sb      $t0, has_respawn_interrupt
+    li      $k0, 1
+    sb      $k0, has_respawn_interrupt
     j       interrupt_dispatch
 
-non_intrpt:                         # was some non-interrupt
-    li      $v0, PRINT_STRING
-    la      $a0, non_intrpt_str
-    syscall                         # print out an error message
-    # fall through to done
-
 done:
-    la      $k0, chunkIH
 
-    # Restore coprocessor1 registers!
-    # If you don't do this and you decide to use division or multiplication
-    #   in your main code, and interrupt handler code, you get WEIRD bugs.
-    lw      $t0, 32($k0)
-    mthi    $t0
-    lw      $t0, 36($k0)
-    mtlo    $t0
-
-    lw      $a0, 0($k0)             # Restore saved registers
-    lw      $v0, 4($k0)
-    lw      $t0, 8($k0)
-    #lw      $t1, 12($k0)
-    #lw      $t2, 16($k0)
-    #lw      $t3, 20($k0)
-    #lw      $t4, 24($k0)
-    #lw      $t5, 28($k0)
+    #Restore at and coprocessor registers
 
 .set noat
-    move    $at, $k1        # Restore $at
+    lw      $at, 0($sp)
 .set at
+
+    lw      $k0, 4($sp)
+    mthi    $k0
+    lw      $k0, 8($sp)
+    mtlo    $k0
+
+    add     $sp, $sp, 12
+
     eret
